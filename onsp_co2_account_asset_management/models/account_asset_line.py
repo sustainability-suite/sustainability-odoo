3131a6a4c27a4cad446931ef9451b037515fa8b2
GCA Open-Net <gca@open-net.ch>
2023-04-04 15:11:50 +0200

    [ADD] onsp_co2_account_asset_management (OCA compatible)

---
+++ onsp_co2_account_asset_management/models/account_asset_line.py
@@ -0,0 +1,9 @@
+from odoo import models
+
+class AccountAssetLine(models.Model):
+    _inherit = "account.asset.line"
+
+    def create_move(self):
+        res = super(AccountAssetLine, self).create_move()
+        self.env['account.move'].browse(res).line_ids._compute_carbon_debt(force_compute=True)
+        return res
--- onsp_co2_mis_builder/__manifest__.py
+++ onsp_co2_mis_builder/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2021 Open Net Sarl
+# © 2023 Open Net Sarl
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
