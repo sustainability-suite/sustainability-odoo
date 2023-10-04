77711bb61c9b244c1bf88bb47aa0a6d7e726f92e
Adam Bonnet <abo@open-net.ch>
2023-10-04 09:54:52 +0200

    [ADD][114069] Implement new GHG, precision value
    MR FIXING
    
    fix(carbon_factor_value.py): add validation checks to ensure non-negative values for CO2e, CH4, N2O, SF6, HFC, PFC, and other GHG values
    fix(carbon_factor_value.py): change field name 'carbon_value_computed' to 'is_carbon_value_computed' for better semantics
    fix(carbon_factor_value.py): change action_reset_precision_value method to use write instead of individual field assignments for better performance
    fix(carbon_factor_value.py): change _compute_is_ghg_detailed_value method to use is_carbon_value_computed field instead of carbon_value_computed field for better semantics
    fix(carbon_factor.xml): change field attributes to use is_carbon_value_computed field instead of carbon_value_computed field for better semantics

--- onsp_co2/models/carbon_factor.py
+++ onsp_co2/models/carbon_factor.py
@@ -2,19 +2,24 @@ from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
-
 class CarbonFactor(models.Model):
     _name = "carbon.factor"
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Carbon Emission Factor"
     _order = "display_name"
     _parent_store = True
 
-
+    ghg_view_mode = fields.Boolean(
+        string="Show greenhouse gases detail", help="Toggle to show GHG details"
+    )
 
     name = fields.Char(required=True)
-    display_name = fields.Char(compute="_compute_display_name", store=True, recursive=True)
-    parent_id = fields.Many2one('carbon.factor', "Parent", index=True, ondelete='restrict')
+    display_name = fields.Char(
+        compute="_compute_display_name", store=True, recursive=True
+    )
+    parent_id = fields.Many2one(
+        "carbon.factor", "Parent", index=True, ondelete="restrict"
+    )
     parent_path = fields.Char(index=True, unaccent=False)
     child_ids = fields.One2many("carbon.factor", "parent_id")
     child_qty = fields.Integer(compute="_compute_child_qty")
@@ -22,8 +27,8 @@ class CarbonFactor(models.Model):
 
     carbon_compute_method = fields.Selection(
         selection=[
-            ('physical', 'Physical'),
-            ('monetary', 'Monetary'),
+            ("physical", "Physical"),
+            ("monetary", "Monetary"),
         ],
         string="Compute method",
     )
@@ -33,20 +38,20 @@ class CarbonFactor(models.Model):
 
     has_invalid_value = fields.Boolean(compute="_compute_has_invalid_value")
 
-
     # related recent values
-    recent_value_id = fields.Many2one("carbon.factor.value", compute="_compute_recent_value", store=True)
+    recent_value_id = fields.Many2one(
+        "carbon.factor.value", compute="_compute_recent_value", store=True
+    )
     carbon_date = fields.Date(related="recent_value_id.date")
     carbon_source = fields.Char(related="recent_value_id.source")
     carbon_value = fields.Float(related="recent_value_id.carbon_value")
     carbon_uom_id = fields.Many2one(related="recent_value_id.carbon_uom_id")
-    carbon_monetary_currency_id = fields.Many2one(related="recent_value_id.carbon_monetary_currency_id")
+    carbon_monetary_currency_id = fields.Many2one(
+        related="recent_value_id.carbon_monetary_currency_id"
+    )
     unit_label = fields.Char(related="recent_value_id.unit_label")
 
-
-
     # --------------------------------------------
-
 
     def name_get(self) -> list[tuple[int, str]]:
         return [(factor.id, factor.display_name) for factor in self]
@@ -57,88 +62,112 @@ class CarbonFactor(models.Model):
         self.ensure_one()
         return f"{self._description}: {self.name}"
 
-
-
-    @api.depends('value_ids.date')
+    @api.depends("value_ids.date")
     def _compute_recent_value(self):
         for factor in self:
-            value_with_dates = factor.value_ids.filtered('date')
-            factor.recent_value_id = value_with_dates and max(value_with_dates, key=lambda f: f.date)
+            value_with_dates = factor.value_ids.filtered("date")
+            factor.recent_value_id = value_with_dates and max(
+                value_with_dates, key=lambda f: f.date
+            )
 
-    @api.depends('child_ids')
+    @api.depends("child_ids")
     def _compute_child_qty(self):
         for factor in self:
             factor.child_qty = len(factor.child_ids)
 
     def _compute_carbon_currency_id(self):
         for factor in self:
-            factor.carbon_currency_id = self.env.ref("onsp_co2.carbon_kilo", raise_if_not_found=False) or self.env['res.currency']
+            factor.carbon_currency_id = (
+                self.env.ref("onsp_co2.carbon_kilo", raise_if_not_found=False)
+                or self.env["res.currency"]
+            )
             factor.carbon_currency_label = factor.carbon_currency_id.currency_unit_label
 
-    @api.depends('parent_id.display_name', 'name')
+    @api.depends("parent_id.display_name", "name")
     def _compute_display_name(self):
         for factor in self:
-            factor.display_name = f"{factor.parent_id.display_name}/{factor.name}" if factor.parent_id else factor.name
+            factor.display_name = (
+                f"{factor.parent_id.display_name}/{factor.name}"
+                if factor.parent_id
+                else factor.name
+            )
 
-    @api.depends('carbon_compute_method', 'carbon_monetary_currency_id', 'carbon_uom_id')
+    @api.depends(
+        "carbon_compute_method", "carbon_monetary_currency_id", "carbon_uom_id"
+    )
     def _compute_unit_label(self):
         for factor in self:
-            if not factor.carbon_compute_method or not (factor.carbon_uom_id or factor.carbon_monetary_currency_id):
+            if not factor.carbon_compute_method or not (
+                factor.carbon_uom_id or factor.carbon_monetary_currency_id
+            ):
                 factor.unit_label = ""
             else:
-                factor.unit_label = "/ " + (factor.carbon_uom_id.name or factor.carbon_monetary_currency_id.currency_unit_label)
+                factor.unit_label = "/ " + (
+                    factor.carbon_uom_id.name
+                    or factor.carbon_monetary_currency_id.currency_unit_label
+                )
 
-
-    @api.depends('carbon_compute_method', 'value_ids.carbon_uom_id', 'value_ids.carbon_monetary_currency_id')
+    @api.depends(
+        "carbon_compute_method",
+        "value_ids.carbon_uom_id",
+        "value_ids.carbon_monetary_currency_id",
+    )
     def _compute_has_invalid_value(self):
         for factor in self:
-            factor.has_invalid_value = (factor.carbon_compute_method == 'physical' and not all([v.carbon_uom_id for v in factor.value_ids])) or \
-                                       (factor.carbon_compute_method == 'monetary' and not all([v.carbon_monetary_currency_id for v in factor.value_ids]))
-
+            factor.has_invalid_value = (
+                factor.carbon_compute_method == "physical"
+                and not all([v.carbon_uom_id for v in factor.value_ids])
+            ) or (
+                factor.carbon_compute_method == "monetary"
+                and not all([v.carbon_monetary_currency_id for v in factor.value_ids])
+            )
 
     def write(self, vals):
-        if (vals.get('carbon_compute_method') == 'physical' and not all([v.carbon_uom_id for v in self.value_ids])) or \
-           (vals.get('carbon_compute_method') == 'monetary' and not all([v.carbon_monetary_currency_id for v in self.value_ids])):
-            raise ValidationError(_("You can not change the compute method if some values miss currency/unit of measure"))
+        if (
+            vals.get("carbon_compute_method") == "physical"
+            and not all([v.carbon_uom_id for v in self.value_ids])
+        ) or (
+            vals.get("carbon_compute_method") == "monetary"
+            and not all([v.carbon_monetary_currency_id for v in self.value_ids])
+        ):
+            raise ValidationError(
+                _(
+                    "You can not change the compute method if some values miss currency/unit of measure"
+                )
+            )
         return super(CarbonFactor, self).write(vals)
-
-
 
     # --------------------------------------------
     #                 Main methods
     # --------------------------------------------
 
-
     def _get_value_at_date(self, date=None):
         self.ensure_one()
         if not date:
             date = fields.Date.today()
-        return self.value_ids and max(self.value_ids.filtered(lambda v: v.date <= date), key=lambda v: v.date)
+        return self.value_ids and max(
+            self.value_ids.filtered(lambda v: v.date <= date), key=lambda v: v.date
+        )
 
     def get_value_infos_at_date(self, date=None) -> dict:
         self.ensure_one()
         value_id = self._get_value_at_date(date)
         return value_id.get_infos_dict()
 
-
-
-
     # --------------------------------------------
     #                   ACTIONS
     # --------------------------------------------
-
 
     def action_see_child_ids(self):
         self.ensure_one()
         return {
             "name": _("Child factors for %s", self.display_name),
-            "type": 'ir.actions.act_window',
-            "res_model": 'carbon.factor',
+            "type": "ir.actions.act_window",
+            "res_model": "carbon.factor",
             "views": [(False, "tree"), (False, "form")],
-            "domain": [('parent_id', '=', self.id)],
-            "target": 'current',
+            "domain": [("parent_id", "=", self.id)],
+            "target": "current",
             "context": {
                 **self.env.context,
             },
         }
-
