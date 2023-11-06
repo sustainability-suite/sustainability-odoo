983c7b1cceaf69eda7a20ed13fc5b44449c98b65
MrFaBemol <mrfabemol@octoopus.org>
2023-11-06 21:43:18 +0100

    [FIX] Remove constraints for companies & make carbon currencies compute

--- onsp_co2/models/res_company.py
+++ onsp_co2/models/res_company.py
@@ -12,8 +12,8 @@ class ResCompany(models.Model):
     carbon_out_value = fields.Float(compute=False, store=True, readonly=False, tracking=True)
     carbon_in_value_origin = fields.Char(compute=False, store=True, related="name")
     carbon_out_value_origin = fields.Char(compute=False, store=True, related="name")
-    carbon_in_monetary_currency_id = fields.Many2one(related="currency_id", store=True, string="CO2e Currency for 'in' moves")
-    carbon_out_monetary_currency_id = fields.Many2one(related="currency_id", store=True, string="CO2e Currency for 'out' moves")
+    carbon_in_monetary_currency_id = fields.Many2one(compute="_compute_carbon_currencies", store=True, string="CO2e Currency for 'in' moves")
+    carbon_out_monetary_currency_id = fields.Many2one(compute="_compute_carbon_currencies", store=True, string="CO2e Currency for 'out' moves")
     carbon_in_compute_method = fields.Selection(selection=[('monetary', 'Monetary')], default='monetary', required=True)
     carbon_out_compute_method = fields.Selection(selection=[('monetary', 'Monetary')], default='monetary', required=True)
 
@@ -31,3 +31,13 @@ class ResCompany(models.Model):
             vals['carbon_in_is_manual'] = True
             vals['carbon_out_is_manual'] = True
         return super(ResCompany, self).create(vals_list)
+
+    # Monkey patch to avoid ValidationError that would lead to incomplete fields
+    def _check_mandatory_fields(self):
+        pass
+
+    @api.depends('currency_id')
+    def _compute_carbon_currencies(self):
+        for company in self:
+            company.carbon_in_monetary_currency_id = company.currency_id
+            company.carbon_out_monetary_currency_id = company.currency_id
