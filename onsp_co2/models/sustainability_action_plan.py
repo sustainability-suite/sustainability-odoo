04dbc3c3817726d4611de35a7de190a556dc8b4e
jacopobacci <bacci.jaco@gmail.com>
2024-04-15 17:12:01 +0200

    feat: add button to see actions

--- onsp_co2/models/sustainability_action_plan.py
+++ onsp_co2/models/sustainability_action_plan.py
@@ -14,3 +14,12 @@ class SustainabilityActionPlan(models.Model):
     scenario_id = fields.Many2one(
         "sustainability.scenario", required=True, ondelete="restrict"
     )
+
+    def action_open_actions(self):
+        self.ensure_one()
+
+        action_plan = self[0]
+
+        action = self.env.ref("onsp_co2.sustainability_action_window_action").read()[0]
+        action["domain"] = [("action_plan_ids", "in", action_plan.ids)]
+        return action
