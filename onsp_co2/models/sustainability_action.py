3a3e0d9f4db238b3e7b5fae6ba452c28bd146f4f
jacopobacci <bacci.jaco@gmail.com>
2024-04-16 08:46:04 +0200

    fix: image field size

--- onsp_co2/models/sustainability_action.py
+++ onsp_co2/models/sustainability_action.py
@@ -17,13 +17,7 @@ class SustainabilityAction(models.Model):
     complexity = fields.Selection(_levels, required=True)
     budget = fields.Selection(_levels, required=True)
     reduction_potential = fields.Selection(_levels, required=True)
-    image_1920 = fields.Image(max_width=1920, max_height=1920, required=False)
-    image_512 = fields.Image(
-        related="image_1920", max_width=512, max_height=512, read_only=True
-    )
-    image_128 = fields.Image(
-        related="image_1920", max_width=128, max_height=128, read_only=True
-    )
+    image = fields.Image(max_width=1920, max_height=1920, required=False)
     active = fields.Boolean(default=True)
 
     action_plan_ids = fields.Many2many("sustainability.action.plan", required=False)
