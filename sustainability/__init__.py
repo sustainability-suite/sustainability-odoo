45f275d53866d8d3910a4907cf83d39945cbaa8f
jacopobacci <bacci.jaco@gmail.com>
2024-07-30 11:24:59 +0200

    fix(sustainability): filter account moves by move_type in post_init_hook to ensure only relevant moves are processed

--- sustainability/__init__.py
+++ sustainability/__init__.py
@@ -8,6 +8,7 @@ from . import tests
 def post_init_hook(env):
     """Create `carbon.line.origin` records if demo data"""
     if env.ref("base.user_demo", raise_if_not_found=False):
-        moves = env["account.move"].search([])
+        move_types = ["out_invoice", "in_invoice", "out_refund", "in_refund"]
+        moves = env["account.move"].search([("move_type", "in", move_types)])
         for move in moves:
             move.action_recompute_carbon()
