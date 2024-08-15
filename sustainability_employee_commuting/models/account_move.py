bae59e2bdfed5a198df3b40e2ebc3aaa4cbbc6be
Bonnet Adam <abo@open-net.ch>
2024-08-15 15:41:31 +0200

    [IMP] sustainability_employee_commuting: Fix cron run and add auto posting when creating account move (#161)

--- sustainability_employee_commuting/models/account_move.py
+++ sustainability_employee_commuting/models/account_move.py
@@ -5,6 +5,11 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     is_employee_commuting_carbon = fields.Boolean(
-        "Employees' commuting carbon emissions", default=False
+        "Employees' commuting carbon emissions", default=False, copy=False
     )
-    employee_commuting_carbon_date = fields.Date("Employee commuting date")
+    employee_commuting_carbon_date = fields.Date(
+        "Employee commuting date", default=False, copy=False
+    )
+
+    def _sustainability_empty_carbon_fields(self):
+        self.is_employee_commuting_carbon = self.employee_commuting_carbon_date = False
