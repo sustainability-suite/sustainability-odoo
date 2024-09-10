c29fbba6020249d71f7f0d042970be4aaec525b2
jacopobacci <bacci.jaco@gmail.com>
2024-09-10 15:07:23 +0200

    fix(migrations): remove obsolete data from ir_model_data in post-migration script to ensure data consistency

--- sustainability/migrations/17.0.1.0.1/post-migration.py
+++ sustainability/migrations/17.0.1.0.1/post-migration.py
@@ -3,3 +3,4 @@ def migrate(cr, version):
     cr.execute("ALTER TABLE carbon_factor DROP COLUMN temp_source_id")
     cr.execute("UPDATE carbon_line_origin SET factor_database_id=temp_source_id")
     cr.execute("ALTER TABLE carbon_line_origin DROP COLUMN temp_source_id")
+    cr.execute("DELETE FROM ir_model_data WHERE model = 'carbon.factor.source'")
