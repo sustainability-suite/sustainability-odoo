ece816f3e534baaa960b526007d315c181fc4894
Julien Guenat <julien@jguenat.ch>
2024-09-29 18:23:16 +0200

    [IMP] sustainability*: adjust authors and contributors

--- sustainability_purchase/__manifest__.py
+++ sustainability_purchase/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sustainability Purchase",
     "version": "17.0.1.0.0",
-    "author": "Open Net Sàrl, Gautier Casabona, Julien Guenat, Adam Bonnet, Jacopo Bacci",
+    "author": "Open Net Sàrl",
     "maintainers": ["jguenat", "bonnetadam", "jacopobacci"],
     "development_status": "Production/Stable",
     "category": "Accounting/Sustainability",
