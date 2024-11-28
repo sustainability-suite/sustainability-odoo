056a1bf7d806ad4f9749616a9106cd35de14f43e
jacopobacci <bacci.jaco@gmail.com>
2024-11-28 10:21:56 +0100

    refactor(sustainability_website_sale): remove unused module and update template logic
    Remove the `sustainability_website_sale` module as it is no longer needed.
    Update the template logic to directly use `carbon_out_factor_id.carbon_value`
    instead of the related field `carbon_out_factor_value` for better clarity
    and maintainability.

--- sustainability_website_sale/__init__.py
+++
@@ -1,4 +0,0 @@
-# © 2021 Open Net Sarl
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import models
--- sustainability_website_sale/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-# from odoo.addons.sustainability.models.carbon_mixin import auto_depends
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    carbon_out_factor_value = fields.Float(related="carbon_out_factor_id.carbon_value")
