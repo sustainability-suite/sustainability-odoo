3f92f0a194357bb929da886d26ffc4279a7c30f3
jacopobacci <bacci.jaco@gmail.com>
2024-12-03 09:40:19 +0100

    feat(models.js): enhance carbon value calculation by considering product quantity and refactor code for clarity and precision

--- sustainability_point_of_sale/static/src/js/models.js
+++ sustainability_point_of_sale/static/src/js/models.js
@@ -5,46 +5,44 @@ import {Order, Orderline} from "@point_of_sale/app/store/models";
 
 patch(Order.prototype, {
     export_for_printing() {
-        let result = super.export_for_printing(...arguments);
-
+        const result = super.export_for_printing(...arguments);
         result.total_carbon_value = this.get_total_carbon_value();
-
         return result;
     },
 
     get_total_carbon_value() {
-        const total = this.orderlines.reduce((sum, line) => sum + line.carbon_value, 0);
-        return (Math.round(total * 100) / 100).toFixed(2);
+        const total = this.orderlines.reduce((sum, line) => sum + line.carbon_value * line.quantity, 0);
+        return total.toFixed(2);
     },
 });
 
+const productQuantities = {};
+
 patch(Orderline.prototype, {
     async setup() {
-        super.setup(...arguments);
+        await super.setup(...arguments);
         this.carbon_value = 0;
 
         const productId = this.product.id;
-        const qty = this.quantity;
+        productQuantities[productId] = (productQuantities[productId] || 0) + 1;
 
         const productRes = await this.env.services.orm.call("product.product", "search_read", [[["id", "=", productId]], ["carbon_out_factor_id"]]);
-
         if (productRes.length === 0) return;
 
         const carbonOutFactor = productRes[0].carbon_out_factor_id;
-
         if (!carbonOutFactor) return;
 
-        const carbonFactorRes = await this.env.services.orm.call("carbon.factor", "search_read", [[["id", "=", carbonOutFactor[0]]], ["carbon_value"]]);
+        const carbonFactor = await this.env.services.orm.call("carbon.factor", "search_read", [[["id", "=", carbonOutFactor[0]]], ["carbon_value"]]);
+        if (carbonFactor.length === 0) return;
 
-        if (carbonFactorRes.length === 0) return;
-
-        const carbonValue = carbonFactorRes[0].carbon_value || 0;
-        this.carbon_value = Math.round(carbonValue * qty * 100) / 100;
+        this.carbon_value = parseFloat(carbonFactor[0].carbon_value || 0).toFixed(2);
     },
+
     getDisplayData() {
+        const qty = productQuantities[this.product.id];
         return {
             ...super.getDisplayData(),
-            carbon_value: this.carbon_value.toFixed(2),
+            carbon_value: (this.carbon_value * qty).toFixed(2),
         };
     },
 });
