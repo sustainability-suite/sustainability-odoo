0398e4a5bd49381b388e415cf794db34e544e4c7
jacopobacci <bacci.jaco@gmail.com>
2024-12-04 16:07:36 +0100

    feat: add sustainability product label print module
    Introduce a new module, sustainability_product_label_print, to enhance
    product labels with CO2e information. This module allows users to
    activate CO2e data on printed labels from the Sustainability settings.
    The addition includes necessary templates and configurations to support
    various label formats, ensuring flexibility and adaptability for
    different printing needs. This enhancement aims to provide more
    transparent and informative product labeling, aligning with
    sustainability goals.

---
+++ sustainability_product_label_print/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Sustainability Product Label Print",
+    "category": "Sales/Sales",
+    "version": "17.0.1.0.0",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+    "author": "Open Net Sàrl",
+    "maintainers": ["jacopobacci"],
+    "development_status": "Production/Stable",
+    "website": "https://www.open-net.ch",
+    "depends": ["sustainability"],
+    "data": [
+        "report/product_product_templates.xml",
+        "report/product_template_templates.xml",
+    ],
+    "sequence": 1,
+}
