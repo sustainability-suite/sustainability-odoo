2d653f1db8e9f258c36caee7a07aa7294c8f8054
Julien Guenat <julien@jguenat.ch>
2024-12-23 17:27:36 +0100

    [FIX] sustainability*: demo data needs fixing

--- sustainability/__manifest__.py
+++ sustainability/__manifest__.py
@@ -59,7 +59,7 @@
         "demo/carbon_factor_type.xml",
         "demo/carbon_factor.xml",
         "demo/carbon_factor_value.xml",
-        "demo/demo.xml",
+        # "demo/demo.xml",
     ],
     "installable": True,
     "application": True,
