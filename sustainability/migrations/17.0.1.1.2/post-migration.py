0b3dec91b41ac069401e4cd6e5920c42123528e1
Adam Bonnet <abo@open-net.ch>
2025-01-08 11:11:56 +0100

    [IMP] sustainability: Fix previous data where carbon is manual but no carbon_factor_id set

---
+++ sustainability/migrations/17.0.1.1.2/post-migration.py
@@ -0,0 +1,28 @@
+from odoo import SUPERUSER_ID, api
+
+from odoo.addons.sustainability.models.carbon_mixin import CARBON_MODELS
+
+
+def migrate(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    models = CARBON_MODELS
+    models.remove("carbon.factor")
+    models.remove("res.partner")
+
+    for model in models:
+        for record in env[model].search(
+            [
+                "|",
+                "|",
+                ("carbon_in_is_manual", "!=", False),
+                ("carbon_in_factor_id", "=", False),
+                "|",
+                ("carbon_out_is_manual", "!=", False),
+                ("carbon_out_factor_id", "=", False),
+            ]
+        ):
+            if record.carbon_in_is_manual and not record.carbon_in_factor_id:
+                record.carbon_in_is_manual = False
+
+            if record.carbon_out_is_manual and not record.carbon_out_factor_id:
+                record.carbon_out_is_manual = False
