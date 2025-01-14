0eff5e9d181bd6af0ccc23d880b7a15a150e505a
jacopobacci <bacci.jaco@gmail.com>
2025-01-14 16:56:36 +0100

    feat(tests): enhance carbon factor setup and add product, account, and invoice creation in tests
    Enhance the test setup by adding more carbon factors, values, and types.
    Introduce a new product, account, and invoice creation to simulate real-world
    scenarios. This improves test coverage and ensures robustness in handling
    various carbon factor configurations and related entities.

--- sustainability/tests/common.py
+++ sustainability/tests/common.py
@@ -8,55 +8,90 @@ class CarbonCommon(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
 
+        # Disable tracking test suite
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.user_model = cls.env["res.users"].with_context(no_reset_password=True)
+
         # UoMs and Currencies
         cls.uom_hour = cls.env.ref("uom.product_uom_hour")
         cls.uom_day = cls.env.ref("uom.product_uom_day")
+        cls.uom_unit = cls.env.ref("uom.product_uom_unit")
         cls.currency_eur = cls.env.ref("base.EUR")
         cls.currency_usd = cls.env.ref("base.USD")
 
-        # Carbon Factors
+        # Carbon Factors and Values
         cls.carbon_factor_default_fallback = cls.env["carbon.factor"].create(
             {
                 "name": "Global Emission Factor Fallback",
                 "carbon_compute_method": "monetary",
             }
         )
-        cls.env["carbon.factor.value"].create(
+        (
+            cls.carbon_factor_monetary,
+            cls.carbon_factor_physical,
+            cls.carbon_factor_plastic_chair,
+        ) = cls.env["carbon.factor"].create(
+            [
+                {"name": "Test monetary", "carbon_compute_method": "monetary"},
+                {"name": "Test physical", "carbon_compute_method": "physical"},
+                {"name": "Plastic chair", "carbon_compute_method": "physical"},
+            ]
+        )
+
+        carbon_values = [
             {
                 "factor_id": cls.carbon_factor_default_fallback.id,
                 "carbon_monetary_currency_id": cls.currency_eur.id,
                 "date": datetime.today().strftime("%Y-%m-%d %H:%M"),
-                "carbon_value": 10.000000,
-            }
-        )
-        cls.carbon_factor_monetary = cls.env["carbon.factor"].create(
-            {
-                "name": "Test monetary",
-                "carbon_compute_method": "monetary",
-            }
-        )
-        cls.env["carbon.factor.value"].create(
+                "carbon_value": 10.0,
+            },
             {
                 "factor_id": cls.carbon_factor_monetary.id,
                 "carbon_monetary_currency_id": cls.currency_eur.id,
                 "date": datetime.today().strftime("%Y-%m-%d %H:%M"),
-                "carbon_value": 0.025000,
-            }
-        )
-        cls.carbon_factor_physical = cls.env["carbon.factor"].create(
-            {
-                "name": "Test physical",
-                "carbon_compute_method": "physical",
-            }
-        )
-        cls.env["carbon.factor.value"].create(
+                "carbon_value": 0.025,
+            },
             {
                 "factor_id": cls.carbon_factor_physical.id,
                 "carbon_uom_id": cls.uom_hour.id,
                 "date": datetime.today().strftime("%Y-%m-%d %H:%M"),
-                "carbon_value": 0.022000,
-            }
+                "carbon_value": 0.022,
+            },
+        ]
+        cls.env["carbon.factor.value"].create(carbon_values)
+
+        carbon_factor_types = cls.env["carbon.factor.type"].create(
+            [
+                {"code": "Type 1", "name": "Type 1"},
+                {"code": "Type 2", "name": "Type 2"},
+                {"code": "Type 3", "name": "Type 3"},
+            ]
         )
+
+        carbon_factor_plastic_values = [
+            {
+                "factor_id": cls.carbon_factor_plastic_chair.id,
+                "carbon_uom_id": cls.uom_unit.id,
+                "date": "2022-01-01",
+                "carbon_value": 10,
+                "type_id": carbon_factor_types[0].id,
+            },
+            {
+                "factor_id": cls.carbon_factor_plastic_chair.id,
+                "carbon_uom_id": cls.uom_unit.id,
+                "date": "2022-01-01",
+                "carbon_value": 10,
+                "type_id": carbon_factor_types[1].id,
+            },
+            {
+                "factor_id": cls.carbon_factor_plastic_chair.id,
+                "carbon_uom_id": cls.uom_unit.id,
+                "date": "2022-01-01",
+                "carbon_value": 20,
+                "type_id": carbon_factor_types[2].id,
+            },
+        ]
+        cls.env["carbon.factor.value"].create(carbon_factor_plastic_values)
 
         # Currency Rates
         cls.env["res.currency.rate"].search([]).unlink()
@@ -77,7 +112,7 @@ class CarbonCommon(TransactionCase):
             ]
         )
 
-        # Company
+        # Company Setup
         cls.env.company.write(
             {
                 "currency_id": cls.currency_usd.id,
@@ -95,6 +130,19 @@ class CarbonCommon(TransactionCase):
             }
         )
 
+        # Office Chair Product
+        cls.office_char_product = cls.env["product.product"].create(
+            {
+                "name": "Office chair",
+                "list_price": 150.00,
+                "standard_price": 100.00,
+                "detailed_type": "consu",
+                "categ_id": cls.product_category.id,
+                "uom_id": cls.uom_unit.id,
+                "carbon_in_factor_id": cls.carbon_factor_plastic_chair.id,
+            }
+        )
+
         # Partner
         cls.partner = cls.env["res.partner"].create(
             {
@@ -107,12 +155,46 @@ class CarbonCommon(TransactionCase):
             }
         )
 
+        # Account
+        cls.revenue_account = cls.env["account.account"].create(
+            {
+                "name": "Test Revenue Account",
+                "code": "REV1234",
+                "account_type": "income",
+                "company_id": cls.env.company.id,
+            }
+        )
+
         # User
-        cls.user = cls.env["res.users"].create(
+        cls.user = cls.user_model.create(
             {
                 "name": "Test User",
                 "login": "test_user",
                 "email": "test.user@example.com",
                 "groups_id": [(6, 0, [cls.env.ref("base.group_user").id])],
+            }
+        )
+
+        # Invoice Creation
+        cls.invoice = cls.env["account.move"].create(
+            {
+                "partner_id": cls.partner.id,
+                "date": datetime.today().strftime("%Y-%m-%d"),
+                "currency_id": cls.currency_eur.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.office_char_product.id,
+                            "quantity": 1.0,
+                            "price_unit": cls.office_char_product.list_price,
+                            "name": cls.office_char_product.name,
+                            "account_id": cls.revenue_account.id,
+                            "product_uom_id": cls.office_char_product.uom_id.id,
+                        },
+                    )
+                ],
             }
         )
