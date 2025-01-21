d12b66d41537ef01df3391483453a7c535d848b2
jacopobacci <bacci.jaco@gmail.com>
2025-01-21 14:10:54 +0100

    test(sustainability): add test for carbon line origin in invoices with manual data
    Add a new test module `test_preceeding_order.py` to verify that
    carbon line origin values are correctly set for invoices with
    manually specified carbon data. This ensures the accuracy of
    carbon computations in the sustainability module.

--- sustainability/tests/__init__.py
+++ sustainability/tests/__init__.py
@@ -1,2 +1,3 @@
 from . import test_conversion
 from . import test_carbon_computation
+from . import test_preceeding_order
---
+++ sustainability/tests/test_preceeding_order.py
@@ -0,0 +1,60 @@
+from odoo.addons.sustainability.tests.common import CarbonCommon
+
+
+class TestPreceedingOrder(CarbonCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Product",
+                "list_price": 50.00,
+                "standard_price": 40.00,
+                "uom_id": cls.uom_hour.id,
+                "carbon_in_factor_id": cls.carbon_factor_physical.id,
+                "carbon_in_is_manual": True,
+            }
+        )
+
+    def test_carbon_on_invoice(self):
+        """Verify carbon line origin values are correctly set for invoices with manually specified carbon data."""
+
+        invoice = self.env["account.move"].create(
+            {
+                "partner_id": self.partner.id,
+                "invoice_date": "2025-01-01",
+                "currency_id": self.currency_usd.id,
+                "move_type": "in_invoice",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product.id,
+                            "quantity": 1,
+                            "price_unit": 40.00,
+                            "carbon_debt": 15.0,
+                            "carbon_uncertainty_value": 0,
+                            "carbon_is_locked": True,
+                            "carbon_data_uncertainty_percentage": 0,
+                            "carbon_origin_json": {
+                                "mode": "manual",
+                            },
+                        },
+                    )
+                ],
+            }
+        )
+        invoice.action_post()
+
+        carbon_line_origin = self.env["carbon.line.origin"].search(
+            [("factor_id", "=", None), ("move_id", "=", invoice.id)], limit=1
+        )
+
+        expected_result = 15.0
+
+        self.assertEqual(
+            carbon_line_origin.signed_value,
+            expected_result,
+            f"Expected a signed value of {expected_result} for the carbon line origin, but the computed value is incorrect.",
+        )
