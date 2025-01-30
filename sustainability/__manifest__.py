15f2ee0e13e3cb850fa5c472dcc077b982a66b89
Adam Bonnet <abo@open-net.ch>
2025-01-30 14:26:50 +0100

    [FIX] sustainability_purchase: Remove on form view

--- sustainability/__manifest__.py
+++ sustainability/__manifest__.py
@@ -32,7 +32,6 @@
         "views/carbon_factor_contributor.xml",
         "views/carbon_factor_type.xml",
         "views/product_category.xml",
-        "views/product_supplierinfo.xml",
         "views/res_country.xml",
         "views/res_config_settings.xml",
         "views/sustainability_scenario.xml",
