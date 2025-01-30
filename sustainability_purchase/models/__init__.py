940e73928eeba38d9a739671d00f66fdba55c520
Jacopo Bacci <77291296+jacopobacci@users.noreply.github.com>
2025-01-30 11:49:26 +0100

    [ADD] sustainability*: product's supplier emission factor preceding order (#223)
    - #201
    
    ---------
    
    Co-authored-by: Adam Bonnet <abo@open-net.ch>

--- sustainability_purchase/models/__init__.py
+++ sustainability_purchase/models/__init__.py
@@ -1,8 +1,4 @@
-# from . import product_supplierinfo
-from . import account_move_line
 from . import product_template
 from . import purchase_order
 from . import purchase_order_line
-from . import res_partner
-from . import carbon_factor
 from . import carbon_line_origin
