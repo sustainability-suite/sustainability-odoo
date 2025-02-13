9496e54f01b2531536d501369c5c6eeb1bbeb108
SustainabilityBot <info@mycompanyco2.org>
2025-02-13 10:03:37 +0000

    [BOT] post-merge updates

--- sustainability_hr_expense_report/__manifest__.py
+++ sustainability_hr_expense_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CO2 : Expense Reports ",
     "summary": "Provide CO2 accounting data for expense reports",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.2.0",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["bonnetadam"],
     "development_status": "Alpha",
