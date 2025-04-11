24e642b9941b6c3490ee0b92a265fc35e0f889b0
Jacopo Bacci <bacci.jaco@gmail.com>
2025-04-11 14:43:52 +0200

    [FIX] sustainability: create_journal_entry test tour

--- sustainability/static/src/js/tours/create_journal_entry.js
+++ sustainability/static/src/js/tours/create_journal_entry.js
@@ -1,47 +1,40 @@
-/** @odoo-module **/
+import {registry} from "@web/core/registry";
+import {stepUtils} from "@web_tour/tour_service/tour_utils";
 
-// MIG18 FIXME
-// import {_t} from "@web/core/l10n/translation";
-// import {registry} from "@web/core/registry";
-// import {stepUtils} from "@web_tour/tour_service/tour_utils";
-
-// registry.category("web_tour.tours").add("create_journal_entry", {
-//     url: "/web",
-//     steps: () => [
-//         ...stepUtils.goToAppSteps("account.menu_finance"),
-//         {
-//             trigger: '.dropdown button[data-menu-xmlid="account.menu_finance_entries"]',
-//             content: "Click on Accounting",
-//             position: "bottom",
-//         },
-//         {
-//             trigger: 'a[data-menu-xmlid="account.menu_action_move_journal_line_form"]',
-//             content: "Click on Journal Entries",
-//             position: "bottom",
-//         },
-//         {
-//             trigger: "button.o_list_button_add",
-//             content: "Create new invoice",
-//             position: "bottom",
-//         },
-//         {
-//             trigger: ".o_field_x2many_list_row_add > a",
-//             content: "Add a line",
-//             position: "bottom",
-//             run: "click",
-//         },
-//         {
-//             trigger: ".o_field_widget[name='account_id'] .o-autocomplete--input",
-//             content: "Open account dropdown",
-//             position: "bottom",
-//             run: "click",
-//         },
-//         {
-//             trigger: ".o-autocomplete--dropdown-item:nth-child(1) > a",
-//             content: "Select account",
-//             position: "bottom",
-//             run: "click",
-//         },
-//         ...stepUtils.saveForm(),
-//     ],
-// });
+registry.category("web_tour.tours").add("create_journal_entry", {
+    url: "/odoo/accounting?debug=1",
+    steps: () => [
+        {
+            trigger: ".o-dropdown[data-menu-xmlid='account\\.menu_finance_entries']",
+            run: "click",
+            content: "Click on Accounting",
+        },
+        {
+            trigger:
+                ".o-dropdown-item[data-menu-xmlid='account\\.menu_action_move_journal_line_form']",
+            run: "click",
+            content: "Click on Journal Entries",
+        },
+        {
+            trigger: ".o_list_button_add",
+            run: "click",
+            content: "Create new invoice",
+        },
+        {
+            trigger: ".o_field_x2many_list_row_add > a",
+            run: "click",
+            content: "Add a line",
+        },
+        {
+            trigger: ".o_field_widget[name='account_id'] .o-autocomplete--input",
+            content: "Open account dropdown",
+            run: "click",
+        },
+        {
+            trigger: ".o-autocomplete--dropdown-item:nth-child(1) > a",
+            content: "Select account",
+            run: "click",
+        },
+        ...stepUtils.saveForm(),
+    ],
+});
