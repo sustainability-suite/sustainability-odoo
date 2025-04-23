f735a4fa83fef71b4dce2d36e788d697ae7dbb41
Jacopo Bacci <bacci.jaco@gmail.com>
2025-04-23 14:23:04 +0200

    [MIG] sustainability_point_of_sale: Migration to 18.0

--- sustainability_point_of_sale/static/src/js/models.js
+++ sustainability_point_of_sale/static/src/js/models.js
@@ -1,90 +1,79 @@
 /** @odoo-module */
 
+import {Orderline} from "@point_of_sale/app/generic_components/orderline/orderline";
+import {PosOrder} from "@point_of_sale/app/models/pos_order";
+import {PosOrderline} from "@point_of_sale/app/models/pos_order_line";
 import {patch} from "@web/core/utils/patch";
-import {Order, Orderline} from "@point_of_sale/app/store/models";
 
 let productQuantities = {};
 
-patch(Order.prototype, {
+patch(PosOrder.prototype, {
     export_for_printing() {
         const result = super.export_for_printing(...arguments);
         result.total_carbon_value = this.get_total_carbon_value();
-
         productQuantities = {};
-
         return result;
     },
+
     get_total_carbon_value() {
-        const total = this.orderlines.reduce(
-            (sum, line) => sum + line.carbon_value * line.quantity,
+        const total = this.lines.reduce(
+            (sum, line) => sum + line.carbon_value * line.get_quantity(),
             0
         );
-        if (total) return total.toFixed(2);
-        return 0;
+        return total ? total.toFixed(2) : 0;
     },
 });
 
-patch(Orderline.prototype, {
-    async setup() {
-        await super.setup(...arguments);
-        await this._setCarbonValue();
-    },
-    /**
-     * Retrieves the product's carbon output factor from the product or its template and sets the `carbon_value`.
-     * The standard retrieval hierarchy (product -> template -> category -> company) is bypassed to ensure greater precision for the POS.
-     * If the carbon factor is not found in the product or template, the value (0) won't be shown.
-     */
-    async _setCarbonValue() {
+patch(PosOrderline.prototype, {
+    setup() {
+        super.setup(...arguments);
         this.carbon_value = 0;
+        this._carbonValueCalculated = false;
+        this._setCarbonValue();
+    },
 
-        const productId = this.product.id;
+    _setCarbonValue() {
+        const productId = this.product_id.id;
         productQuantities[productId] = (productQuantities[productId] || 0) + 1;
 
-        const productRes = await this.env.services.orm.searchRead(
-            "product.product",
-            [["id", "=", productId]],
-            ["carbon_out_factor_id", "product_tmpl_id"]
-        );
-        if (productRes.length === 0) return;
-
-        let carbonOutFactor = productRes[0].carbon_out_factor_id;
-
-        if (!carbonOutFactor) {
-            const productTemplate = productRes[0].product_tmpl_id;
-            const productTemplateRes = await this.env.services.orm.searchRead(
-                "product.template",
-                [["id", "=", productTemplate[0]]],
-                ["carbon_out_factor_id"]
-            );
-
-            if (productTemplateRes.length === 0) return;
-
-            carbonOutFactor = productTemplateRes[0].carbon_out_factor_id;
+        if (
+            this.product_id.carbon_out_factor_id &&
+            this.product_id.carbon_out_factor_id.carbon_value
+        ) {
+            this.carbon_value = this.product_id.carbon_out_factor_id.carbon_value;
+        } else {
+            this.carbon_value = 0;
         }
-
-        if (!carbonOutFactor) return;
-
-        const carbonFactor = await this.env.services.orm.searchRead(
-            "carbon.factor",
-            [["id", "=", carbonOutFactor[0]]],
-            ["carbon_value"]
-        );
-        if (carbonFactor.length === 0) return;
-
-        this.carbon_value = parseFloat(carbonFactor[0].carbon_value || 0).toFixed(2);
     },
-    getDisplayData() {
-        const qty = productQuantities[this.product.id];
-        const data = super.getDisplayData();
 
-        if (this.carbon_value) data.carbon_value = (this.carbon_value * qty).toFixed(2);
+    getDisplayData() {
+        const data = super.getDisplayData();
+        const qty = productQuantities[this.product_id.id] || this.get_quantity();
+        const carbonTotal = parseFloat(this.carbon_value) * qty;
+
+        if (carbonTotal > 0) {
+            data.carbon_value = carbonTotal.toFixed(2);
+        }
 
         return data;
     },
+
     export_as_JSON() {
         const json = super.export_as_JSON(...arguments);
-        const productId = json.product_id;
-        productQuantities[productId] = json.qty;
+        productQuantities[json.product_id] = json.qty;
         return json;
+    },
+});
+
+patch(Orderline, {
+    props: {
+        ...Orderline.props,
+        line: {
+            ...Orderline.props.line,
+            shape: {
+                ...Orderline.props.line.shape,
+                carbon_value: {type: String, optional: true},
+            },
+        },
     },
 });
