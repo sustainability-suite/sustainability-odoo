ad75ca943a510d38e7673f15f6c1fb5b470387a1
Adam Bonnet <abo@open-net.ch>
2025-04-30 11:16:11 +0200

    [IMP] sustainability: Add Carbon Footprint on account account

--- sustainability/models/account_account.py
+++ sustainability/models/account_account.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 
 class AccountAccount(models.Model):
     _name = "account.account"
-    _inherit = ["account.account", "carbon.mixin"]
+    _inherit = ["account.account", "carbon.mixin", "carbon.common.mixin"]
     _carbon_types = ["in"]
 
     @api.model
@@ -15,3 +15,20 @@ class AccountAccount(models.Model):
     carbon_in_factor_id = fields.Many2one(
         string="Emission Factor Purchases", tracking=True
     )
+
+    carbon_line_origin_ids = fields.One2many(
+        comodel_name="carbon.line.origin",
+        inverse_name="move_line_account_id",
+        string="Origins",
+    )
+    carbon_line_origin_qty = fields.Integer(compute="_compute_carbon_line_origin_qty")
+
+    def action_see_carbon_line_origin_ids(self):
+        return self._generate_action(
+            model="carbon.line.origin",
+            ids=self.carbon_line_origin_ids.ids,
+        )
+
+    def _compute_carbon_line_origin_qty(self):
+        for factor in self:
+            factor.carbon_line_origin_qty = len(self.carbon_line_origin_ids)
