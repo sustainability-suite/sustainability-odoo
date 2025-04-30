24dd51426cf5dff40cecf25fae2ba6c7760cb21a
Adam Bonnet <abo@open-net.ch>
2025-04-30 11:13:12 +0200

    [IMP] sustainability: Add Carbon Footprint on product template

--- sustainability/models/product_template.py
+++ sustainability/models/product_template.py
@@ -1,14 +1,31 @@
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
     _name = "product.template"
-    _inherit = ["product.template", "carbon.mixin"]
+    _inherit = ["product.template", "carbon.mixin", "carbon.common.mixin"]
 
     """
     Add fallback values if product value missing with the following priority order:
         - Product category
     """
+
+    carbon_line_origin_ids = fields.One2many(
+        comodel_name="carbon.line.origin",
+        inverse_name="move_line_product_id",
+        string="Origins",
+    )
+    carbon_line_origin_qty = fields.Integer(compute="_compute_carbon_line_origin_qty")
+
+    def action_see_carbon_line_origin_ids(self):
+        return self._generate_action(
+            model="carbon.line.origin",
+            ids=self.carbon_line_origin_ids.ids,
+        )
+
+    def _compute_carbon_line_origin_qty(self):
+        for factor in self:
+            factor.carbon_line_origin_qty = len(self.carbon_line_origin_ids)
 
     def _get_carbon_in_fallback_records(self):
         res = super()._get_carbon_in_fallback_records()
