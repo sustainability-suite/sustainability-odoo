4a8145dee91d963c36c8766dcb39c9716ea3241e
SustainabilityBot <info@mycompanyco2.org>
2025-07-28 09:17:50 +0000

    [BOT] post-merge updates

--- sustainability_employee_commuting/__manifest__.py
+++ sustainability_employee_commuting/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Sustainability Employee Commuting",
-    "version": "17.0.1.1.1",
+    "version": "17.0.1.1.2",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["bonnetadam", "jacopobacci"],
     "development_status": "Production/Stable",
