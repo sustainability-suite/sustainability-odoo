799556e34f2ecd07427b161d481489d82e8780fd
Adam Bonnet <abo@open-net.ch>
2025-07-28 11:28:31 +0200

    sustainability: Update migrations script

--- sustainability/__manifest__.py
+++ sustainability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sustainability",
-    "version": "16.0.1.9.2",
+    "version": "16.0.1.9.3",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["jguenat", "bonnetadam", "jacopobacci"],
     "development_status": "Production/Stable",
