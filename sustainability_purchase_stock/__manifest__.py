a903670ba58e58ae33fd456dc415bfd5d477161e
SustainabilityBot <info@mycompanyco2.org>
2025-07-29 13:37:03 +0000

    [BOT] post-merge updates

--- sustainability_purchase_stock/__manifest__.py
+++ sustainability_purchase_stock/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sustainability Purchase Stock",
     "category": "Purchase",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["jacopobacci"],
     "development_status": "Production/Stable",
