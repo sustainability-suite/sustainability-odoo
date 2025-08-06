2cc880a1d93b89a7e4155575235272069190d0f5
Adam Bonnet <abo@open-net.ch>
2025-08-06 11:38:46 +0200

    [REF] sustainability_purchase: Adapt code to view management changes

--- sustainability_purchase/__manifest__.py
+++ sustainability_purchase/__manifest__.py
@@ -15,7 +15,6 @@
         # Data
         "data/ir_cron.xml",
         # Views
-        "views/carbon_factor.xml",
         "views/carbon_line_origin.xml",
         "views/purchase_order.xml",
         "views/purchase_supplierinfo.xml",
