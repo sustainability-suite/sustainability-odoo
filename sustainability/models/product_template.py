7c43d44bd734416fb8ed872bd65d107c81449cda
Adam Bonnet <abo@open-net.ch>
2025-08-06 15:51:40 +0200

    [IMP] sustainability: Enable distribution and distribution template on product product and product template

--- sustainability/models/product_template.py
+++ sustainability/models/product_template.py
@@ -4,6 +4,8 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _name = "product.template"
     _inherit = ["product.template", "carbon.mixin"]
+    _carbon_enable_distribution = True
+    _carbon_enable_distribution_template = True
     _fallback_records = ["categ_id"]
 
     """
