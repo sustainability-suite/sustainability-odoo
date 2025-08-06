a82a41b06219692a55797b271cbe0416a4cd3f61
Adam Bonnet <abo@open-net.ch>
2025-08-06 11:38:33 +0200

    [REF] sustainability: Adapt code to view management changes

--- sustainability/models/res_partner.py
+++ sustainability/models/res_partner.py
@@ -9,6 +9,7 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _name = "res.partner"
     _inherit = ["res.partner", "carbon.mixin"]
+    _carbon_types = ["in"]
     _carbon_enable_page = False
 
     @api.model
