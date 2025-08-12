0e1bc12f1c9973c4b14daaed20247aaca95a1138
SustainabilityBot <info@mycompanyco2.org>
2025-08-12 07:46:42 +0000

    [BOT] post-merge updates

--- sustainability/__manifest__.py
+++ sustainability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sustainability",
-    "version": "18.0.1.5.1",
+    "version": "18.0.1.5.2",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["jguenat", "bonnetadam", "jacopobacci"],
     "development_status": "Production/Stable",
