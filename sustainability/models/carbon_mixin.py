a64f7d5ca2c0b05abff345a45a7516d4ebaf3be1
Adam Bonnet <abo@open-net.ch>
2025-08-25 14:43:10 +0200

    [FIX] sustainability: Fix UOM domain bug. Fix in product product

--- sustainability/models/carbon_mixin.py
+++ sustainability/models/carbon_mixin.py
@@ -5,6 +5,7 @@ from lxml import etree
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
+from odoo.tools.misc import unquote
 
 from .carbon_factor import CarbonFactor
 
@@ -104,8 +105,29 @@ class CarbonMixin(models.AbstractModel):
             ("recent_value_id", "!=", False),
         ]
 
-    def _get_uom_filtered_factors_domain(self, uom_id):
-        """Filter physical EF on product uom & weight + include monetary ones."""
+    def _get_uom_filtered_factors_domain(self, uom_field=None):
+        """Filter physical EF on product uom & weight + include monetary ones. Argument uom_field should be str value."""
+        uom_id = False
+        if uom_field and (
+            isinstance(uom_field, str) and not isinstance(uom_field, unquote)
+        ):
+            uom_id = unquote(uom_field)
+
+        if not uom_id:
+            if "uom_id" in self._fields:
+                uom_id = unquote("uom_id")
+
+        if not isinstance(uom_id, unquote):
+            raise ValidationError(
+                _(
+                    "uom_id for _get_uom_filtered_factors_domain should be a string or unquoted value"
+                )
+            )
+
+        if not uom_id:
+            raise ValidationError(
+                _("A uom_id must be provided for _get_uom_filtered_factors_domain")
+            )
 
         weight_uom_category = self.env.ref("uom.product_uom_categ_kgm")
         return [
