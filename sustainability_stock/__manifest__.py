11f41a2205aeec28e72a2759f9f8800b0ac42e53
Adam Bonnet <abo@open-net.ch>
2025-08-26 11:54:54 +0200

    [UPDT] sustainability_stock: Add Adam as maintainer

--- sustainability_stock/__manifest__.py
+++ sustainability_stock/__manifest__.py
@@ -3,7 +3,7 @@
     "category": "Inventory/Inventory",
     "version": "18.0.1.1.1",
     "author": "MCO2, Open Net Sàrl",
-    "maintainers": ["jacopobacci"],
+    "maintainers": ["jacopobacci", "bonnetadam"],
     "development_status": "Production/Stable",
     "website": "https://github.com/sustainability-suite/sustainability-odoo",
     "depends": ["sustainability", "stock", "stock_delivery"],
