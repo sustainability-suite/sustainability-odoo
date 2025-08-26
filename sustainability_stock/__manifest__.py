d4b55af2904c53da64b6c0f7a037dd304d54342b
Adam Bonnet <abo@open-net.ch>
2025-08-26 10:30:42 +0200

    [FIX] sustainability_stock: Change version and migrations version

--- sustainability_stock/__manifest__.py
+++ sustainability_stock/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sustainability Inventory",
     "category": "Inventory/Inventory",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.1.1",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["jacopobacci"],
     "development_status": "Production/Stable",
