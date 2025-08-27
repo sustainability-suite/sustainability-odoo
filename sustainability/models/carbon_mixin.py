c24a4c62424f2f4107cae2d31f076e41f55c23f8
Adam Bonnet <abo@open-net.ch>
2025-08-27 11:44:05 +0200

    [IMP] sustainability: Add a way to force uom_id for filtered_factors_domain

--- sustainability/models/carbon_mixin.py
+++ sustainability/models/carbon_mixin.py
@@ -130,29 +130,39 @@ class CarbonMixin(models.AbstractModel):
             ("recent_value_id", "!=", False),
         ]
 
-    def _get_uom_filtered_factors_domain(self, uom_field=None):
-        """Filter physical EF on product uom & weight + include monetary ones. Argument uom_field should be str value."""
-        uom_id = False
-        if uom_field and (
-            isinstance(uom_field, str) and not isinstance(uom_field, unquote)
-        ):
-            uom_id = unquote(uom_field)
-
+    def _get_uom_filtered_factors_domain(self, uom_field=None, uom_id=None):
+        """Filter physical EF on product uom & weight + include monetary ones. Argument uom_field should be str value. If uom_id is provided, it will be used instead of uom_field."""
         if not uom_id:
-            if "uom_id" in self._fields:
-                uom_id = unquote("uom_id")
+            uom_id = False
+            if uom_field and (
+                isinstance(uom_field, str) and not isinstance(uom_field, unquote)
+            ):
+                uom_id = unquote(uom_field)
 
-        if not isinstance(uom_id, unquote):
-            raise ValidationError(
-                _(
-                    "uom_id for _get_uom_filtered_factors_domain should be a string or unquoted value"
+            if not uom_id:
+                if "uom_id" in self._fields:
+                    uom_id = unquote("uom_id")
+
+            if not isinstance(uom_id, unquote):
+                raise ValidationError(
+                    _(
+                        "uom_id for _get_uom_filtered_factors_domain should be a string or unquoted value"
+                    )
                 )
-            )
 
-        if not uom_id:
-            raise ValidationError(
-                _("A uom_id must be provided for _get_uom_filtered_factors_domain")
-            )
+            if not uom_id:
+                raise ValidationError(
+                    _("A uom_id must be provided for _get_uom_filtered_factors_domain")
+                )
+
+            if str(uom_id) not in self._fields:
+                raise ValidationError(
+                    _(
+                        "Field %s not exists in model %s",
+                        uom_id,
+                        self._name,
+                    )
+                )
 
         weight_uom_category = self.env.ref("uom.product_uom_categ_kgm")
         return [
