5b0a93b84ecf4091537c6af60d6acead03946800
SustainabilityBot <info@mycompanyco2.org>
2025-09-09 07:47:05 +0000

    [BOT] post-merge updates

--- sustainability_stock/__manifest__.py
+++ sustainability_stock/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sustainability Inventory",
     "category": "Inventory/Inventory",
-    "version": "16.0.1.0.2",
+    "version": "16.0.2.0.0",
     "author": "MCO2, Open Net Sàrl",
     "maintainers": ["jacopobacci"],
     "development_status": "Production/Stable",
